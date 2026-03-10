54a63b6707b6c38a482d5aad15a4817062af8e3c
remacutetigisti <rajeeva.moorthy@gmail.com>
2026-03-10 17:46:15 +0800

    Add ride_hail adapter identity tests

---
+++ tests/test_ride_hail_adapter_identity.py
@@ -0,0 +1,44 @@
+from apps.ride_hail import (
+    AssignmentAgentIndie,
+    AssignmentApp,
+    DriverAgentIndie,
+    DriverApp,
+    PassengerAgentIndie,
+    PassengerApp,
+    RideHailAnalyticsAdapter,
+    RideHailAssignmentAdapter,
+    RideHailDriverAdapter,
+    RideHailPassengerAdapter,
+)
+from apps.ride_hail.adapters import (
+    RideHailAnalyticsAdapter as AnalyticsAdapterFromAdapters,
+)
+from apps.ride_hail.adapters import (
+    RideHailAssignmentAdapter as AssignmentAdapterFromAdapters,
+)
+from apps.ride_hail.adapters import RideHailDriverAdapter as DriverAdapterFromAdapters
+from apps.ride_hail.adapters import (
+    RideHailPassengerAdapter as PassengerAdapterFromAdapters,
+)
+from apps.ride_hail.analytics import AnalyticsAgentIndie, AnalyticsApp
+
+
+def test_ride_hail_adapter_exports_are_consistent_between_packages():
+    assert RideHailAssignmentAdapter is AssignmentAdapterFromAdapters
+    assert RideHailAnalyticsAdapter is AnalyticsAdapterFromAdapters
+    assert RideHailDriverAdapter is DriverAdapterFromAdapters
+    assert RideHailPassengerAdapter is PassengerAdapterFromAdapters
+
+
+def test_ride_hail_adapters_resolve_exact_canonical_class_objects():
+    assert RideHailAssignmentAdapter.get_app_class() is AssignmentApp
+    assert RideHailAssignmentAdapter.get_agent_class() is AssignmentAgentIndie
+
+    assert RideHailAnalyticsAdapter.get_app_class() is AnalyticsApp
+    assert RideHailAnalyticsAdapter.get_agent_class() is AnalyticsAgentIndie
+
+    assert RideHailDriverAdapter.get_app_class() is DriverApp
+    assert RideHailDriverAdapter.get_agent_class() is DriverAgentIndie
+
+    assert RideHailPassengerAdapter.get_app_class() is PassengerApp
+    assert RideHailPassengerAdapter.get_agent_class() is PassengerAgentIndie
