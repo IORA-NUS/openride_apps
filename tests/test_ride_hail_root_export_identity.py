ccbb45ecf89f15c667dd135a5f6f88f98564c494
remacutetigisti <rajeeva.moorthy@gmail.com>
2026-03-10 17:50:04 +0800

    Extend ride_hail root role export identity tests

--- tests/test_ride_hail_root_export_identity.py
+++ tests/test_ride_hail_root_export_identity.py
@@ -1,15 +1,23 @@
 import apps.ride_hail as ride_hail
+from apps.ride_hail.analytics import AnalyticsAgentIndie, AnalyticsApp
 from apps.ride_hail.adapters import (
     RideHailAnalyticsAdapter,
     RideHailAssignmentAdapter,
     RideHailDriverAdapter,
     RideHailPassengerAdapter,
 )
+from apps.ride_hail.assignment import AssignmentAgentIndie, AssignmentApp
 from apps.ride_hail.contracts import (
     validate_assigned_payload,
     validate_driver_workflow_payload,
     validate_passenger_workflow_payload,
     validate_requested_trip_payload,
+)
+from apps.ride_hail.driver import (
+    DriverAgentIndie,
+    DriverApp,
+    DriverManager,
+    DriverTripManager,
 )
 from apps.ride_hail.events import RideHailActions, RideHailEvents
 from apps.ride_hail.models import (
@@ -17,6 +25,12 @@ from apps.ride_hail.models import (
     DriverWorkflowPayload,
     PassengerWorkflowPayload,
     RequestedTripPayload,
+)
+from apps.ride_hail.passenger import (
+    PassengerAgentIndie,
+    PassengerApp,
+    PassengerManager,
+    PassengerTripManager,
 )
 
 
@@ -45,3 +59,21 @@ def test_ride_hail_root_reexports_identity_for_adapter_symbols():
     assert ride_hail.RideHailAnalyticsAdapter is RideHailAnalyticsAdapter
     assert ride_hail.RideHailDriverAdapter is RideHailDriverAdapter
     assert ride_hail.RideHailPassengerAdapter is RideHailPassengerAdapter
+
+
+def test_ride_hail_root_reexports_identity_for_role_class_symbols():
+    assert ride_hail.DriverApp is DriverApp
+    assert ride_hail.DriverAgentIndie is DriverAgentIndie
+    assert ride_hail.DriverManager is DriverManager
+    assert ride_hail.DriverTripManager is DriverTripManager
+
+    assert ride_hail.PassengerApp is PassengerApp
+    assert ride_hail.PassengerAgentIndie is PassengerAgentIndie
+    assert ride_hail.PassengerManager is PassengerManager
+    assert ride_hail.PassengerTripManager is PassengerTripManager
+
+    assert ride_hail.AssignmentApp is AssignmentApp
+    assert ride_hail.AssignmentAgentIndie is AssignmentAgentIndie
+
+    assert ride_hail.AnalyticsApp is AnalyticsApp
+    assert ride_hail.AnalyticsAgentIndie is AnalyticsAgentIndie
