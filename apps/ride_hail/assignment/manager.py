d34217ac8ece840261372878cf0b5cf4e2289605
remacutetigisti <rajeeva.moorthy@gmail.com>
2026-03-10 20:58:00 +0800

    Remove impl import boundaries test and update manager modules

--- apps/ride_hail/assignment/manager.py
+++ apps/ride_hail/assignment/manager.py
@@ -1,3 +1,92 @@
-from .manager_impl import AssignmentManager
+import requests, json
+from http import HTTPStatus
 
-__all__ = ["AssignmentManager"]
+import logging
+from apps.config import settings
+from apps.utils import id_generator, is_success
+from apps.state_machine import WorkflowStateMachine
+
+
+class AssignmentManager():
+
+    def __init__(self, run_id, sim_clock, user, solver):
+        self.run_id = run_id
+        self.user = user
+        self.solver = solver
+
+        # try:
+        self.engine = self.init_engine(sim_clock)
+        # except Exception as e:
+        #     logging.exception(str(e))
+        #     raise e
+            # print(e)
+
+    def as_dict(self):
+        return self.engine
+
+    def get_id(self):
+        return self.engine['_id']
+
+
+    def init_engine(self, sim_clock):
+        engine_url = f"{settings['OPENRIDE_SERVER_URL']}/{self.run_id}/engine"
+
+        params = {
+            'where': json.dumps({
+                'name': self.solver.params['planning_area']['name']
+            })
+        }
+        response = requests.get(engine_url, headers=self.user.get_headers(), params=params)
+
+        if len(response.json()['_items']) == 0:
+            # Need to register and actvate engine
+            response = self.create_engine(sim_clock)
+            return self.init_engine(sim_clock)
+
+        else:
+            return response.json()['_items'][0]
+
+
+    def create_engine(self, sim_clock):
+        engine_url = f"{settings['OPENRIDE_SERVER_URL']}/{self.run_id}/engine"
+        # print(f"{self.solver.__class__.__name__=}")
+        data = {
+            'name': self.solver.params['planning_area']['name'],
+            'strategy': self.solver.__class__.__name__,
+            'planning_area': self.solver.params['planning_area'],
+            'offline_params': self.solver.params['offline_params'],
+            'online_params': self.solver.params['online_params'],
+        }
+
+        response =  requests.post(engine_url, headers=self.user.get_headers(), data=json.dumps(data))
+        # print(response.text)
+        return response
+
+    def update_engine(self, sim_clock, online_params, performance):
+
+        engine_url = f"{settings['OPENRIDE_SERVER_URL']}/{self.run_id}/engine/{self.engine['_id']}"
+
+        data = {
+            "online_params": online_params,
+            "last_run_performance": performance,
+            "sim_clock": sim_clock,
+        }
+        response = requests.patch(engine_url, headers=self.user.get_headers(etag=self.engine['_etag']), data=json.dumps(data))
+
+        if not is_success(response.status_code):
+            logging.warning(f"Update Engine Failed, {response.text}")
+        # print(response.text)
+
+
+    def refresh(self):
+        engine_url = f"{settings['OPENRIDE_SERVER_URL']}/{self.run_id}/engine"
+        engine_item_url = engine_url + f"/{self.engine['_id']}"
+
+        response = requests.get(engine_item_url, headers=self.user.get_headers())
+
+        self.engine = response.json()
+
+
+
+# Backward compatibility for any code importing the old class name.
+EngineManager = AssignmentManager
--- apps/ride_hail/driver/manager.py
+++ apps/ride_hail/driver/manager.py
@@ -1,3 +1,179 @@
-from .manager_impl import DriverManager
+from dateutil.relativedelta import relativedelta
+import requests, json, logging, traceback
+from http import HTTPStatus
 
-__all__ = ["DriverManager"]
+from apps.config import settings
+from apps.utils import id_generator, is_success
+from apps.agent_core.lifecycle import LifecycleManagerBase
+
+
+class DriverManager(LifecycleManagerBase):
+
+    def __init__(self, run_id, sim_clock, user, profile):
+        self.run_id = run_id
+        self.user = user
+        self.profile = profile
+
+        # try:
+        self.driver = self.init_driver(sim_clock)
+        # except Exception as e:
+        #     logging.exception(traceback.format_exc())
+            # print(e)
+
+        self.vehicle = self.init_vehicle(sim_clock)
+
+    def as_dict(self):
+        return self.driver
+
+    def get_id(self):
+        return self.driver['_id']
+
+
+    def init_driver(self, sim_clock):
+        ''' Get / Create the driver record '''
+
+        driver_url = f"{settings['OPENRIDE_SERVER_URL']}/{self.run_id}/driver"
+
+        response = requests.get(driver_url, headers=self.user.get_headers(), timeout=settings.get('NETWORK_REQUEST_TIMEOUT', 10))
+        # print(response.json())
+
+        if is_success(response.status_code):
+            if len(response.json()['_items']) == 0:
+                # Need to register and actvate driver
+                response = self.create_driver(sim_clock)
+                return self.init_driver(sim_clock)
+            else:
+                driver = response.json()['_items'][0]
+        else:
+            raise Exception(f"{response.url}, {response.text}")
+
+        # if len(response.json()['_items']) == 0:
+        #     # Need to register and actvate driver
+        #     response = self.create_driver(sim_clock)
+        #     return self.init_driver(sim_clock)
+        # else:
+        #     driver = response.json()['_items'][0]
+
+        return driver
+
+    def estimate_next_event_time(self, current_time):
+        ''' Return a distant future data as a placeholder'''
+        # Ideally checkdriver status and return a distant future if logged out and a realistic value if logged in
+        return current_time + relativedelta(years=1)
+
+    def create_driver(self, sim_clock):
+        create_driver_url = f"{settings['OPENRIDE_SERVER_URL']}/{self.run_id}/driver"
+
+        data = {
+            "license": {
+                "num": id_generator(), # NOt Guarunteed to be Unique. So need to take care of possible exceptions
+                "country": "Singapore",
+                "expiry":  "Tue, 01 Jan 2030 00:00:00 GMT"
+            },
+            "profile": self.profile,
+            "sim_clock": sim_clock,
+        }
+
+        return requests.post(create_driver_url, headers=self.user.get_headers(), data=json.dumps(data), timeout=settings.get('NETWORK_REQUEST_TIMEOUT', 10))
+
+
+    def login(self, sim_clock):
+        ''' Assuming that init_driver is already called (i.e. self.driver exists)
+            - Ensures that driver is logged in
+            - NOTE: driver must be logged in before creating a trip (empty or occupied)
+        NOTE: this method can be simplified by leveraging on the statemachine
+        '''
+        driver_url = f"{settings['OPENRIDE_SERVER_URL']}/{self.run_id}/driver"
+
+        if self.driver['state'] == 'dormant':
+            self.driver = self._transition_item_to_state(
+                driver_url,
+                self.driver,
+                sim_clock,
+                'offline',
+            )
+
+        if self.driver['state'] == 'offline':
+            self.driver = self._transition_item_to_state(
+                driver_url,
+                self.driver,
+                sim_clock,
+                'online',
+            )
+
+        if self.driver['state'] == 'online':
+
+            return None
+        else:
+            raise Exception ("unknown Workflow State")
+
+    def logout(self, sim_clock):
+        ''' Ensure proper logout procedure is followed.
+        - Once logged out, Driver will be unable to create / update trips
+        '''
+        item_url = f"{settings['OPENRIDE_SERVER_URL']}/{self.run_id}/driver/{self.get_id()}"
+
+        data = {
+            "transition": "logout",
+            "sim_clock": sim_clock,
+        }
+
+        response = requests.patch(item_url,
+                        headers=self.user.get_headers(etag=self.driver['_etag']),
+                        data=json.dumps(data)) # no timeout
+
+        if is_success(response.status_code):
+            self.refresh()
+        else:
+            raise Exception(f"{response.url}, {response.text}")
+
+
+    def init_vehicle(self, sim_clock):
+        vehicle_url = f"{settings['OPENRIDE_SERVER_URL']}/{self.run_id}/vehicle"
+
+        response = requests.get(vehicle_url, headers=self.user.get_headers(), timeout=settings.get('NETWORK_REQUEST_TIMEOUT', 10))
+
+        if len(response.json()['_items']) == 0:
+            # Need to register and actvate vehicle
+            response = self.create_vehicle(sim_clock)
+            return self.init_vehicle(sim_clock)
+
+        else:
+            vehicle = response.json()['_items'][0]
+
+        if vehicle['state'] == 'offline':
+            return vehicle
+        else:
+            return self._transition_item_to_state(
+                vehicle_url,
+                vehicle,
+                sim_clock,
+                'offline',
+            )
+
+    def create_vehicle(self, sim_clock):
+        vehicle_url = f"{settings['OPENRIDE_SERVER_URL']}/{self.run_id}/vehicle"
+
+        data = {
+            "registration": {
+                "num": id_generator(6), # NOT Guarunteed to be Unique. So need to take care of possible exceptions
+                "country": "Singapore",
+                "expiry":  "Tue, 01 Jan 2030 00:00:00 GMT"
+            },
+        	"capacity": 4,
+            "sim_clock": sim_clock,
+        }
+
+        response =  requests.post(vehicle_url, headers=self.user.get_headers(), data=json.dumps(data), timeout=settings.get('NETWORK_REQUEST_TIMEOUT', 10))
+        # print(response.text)
+        return response
+
+    def refresh(self):
+        driver_item_url = f"{settings['OPENRIDE_SERVER_URL']}/{self.run_id}/driver/{self.driver['_id']}"
+
+        response = requests.get(driver_item_url, headers=self.user.get_headers(), timeout=settings.get('NETWORK_REQUEST_TIMEOUT', 10))
+
+        if is_success(response.status_code):
+            self.driver = response.json()
+        else:
+            raise Exception(f'DriverManager.refresh: Failed getting response for {self.driver["_id"]} Got {response.text}')
